2699241507545998979b9dc7c54266f02d32aa67
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-05-08 22:38:44 +0300

    Minor edit.

--- adwm.py
+++ adwm.py
@@ -2,8 +2,8 @@
 """ Copyright© 2023-2024 LinuxUsersLinuxMint
 AdWMGUI Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 AdWMGUI All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint """
+Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/AdWMGUI
+A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/AdWMGUI """
 
 from tkinter import messagebox
 import AdWMFuncLib.Systemclean
